d913ce03201591ded4208f8e0ade60d3550e0fa8
Juwan Wheatley <datwheat@gmail.com>
2019-10-09 11:58:32 -0700

    [dx] add eslint rn plugin and remove styled-components to reduce styling overhead

--- screens/404.tsx
+++ screens/404.tsx
@@ -1,49 +1,47 @@
 import React from 'react';
-import styled from 'styled-components/native';
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 import { fi, majorThird } from '../styles/typography';
 import { useHistory } from '../navigation';
 import { white, black } from '../styles/colors';
 
-const Container = styled(View)`
-  flex: 1;
-  justify-content: center;
-  align-items: center;
-`;
-
-const ErrorCode = styled(Text)`
-  font-family: 'Inter Semibold';
-  font-size: ${majorThird(5)}px;
-  margin-bottom: ${fi(3)}px;
-`;
-
 function Button({ label, onPress, style }) {
   return (
-    <ButtonTouchable activeOpacity={0.8} onPress={onPress} style={style}>
-      <ButtonText>{label}</ButtonText>
-    </ButtonTouchable>
+    <TouchableOpacity activeOpacity={0.8} onPress={onPress} style={[styles.buttonTouchable, style]}>
+      <Text style={styles.buttonText}>{label}</Text>
+    </TouchableOpacity>
   );
 }
-
-const ButtonTouchable = styled(TouchableOpacity)`
-  padding: ${fi(1)}px;
-  border-radius: ${fi(0.5)}px;
-  background-color: ${black()};
-`;
-
-const ButtonText = styled(Text)`
-  font-size: ${majorThird(1)}px;
-  font-family: 'Inter Medium';
-  color: ${white()};
-`;
 
 export default function Error404() {
   const history = useHistory();
 
   return (
-    <Container>
-      <ErrorCode>404</ErrorCode>
+    <View style={styles.container}>
+      <Text style={styles.errorCode}>404</Text>
       <Button onPress={() => history.replace('/')} label="Return to Index" activeOpacity={0.8} />
-    </Container>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  buttonText: {
+    color: white(),
+    fontFamily: 'Inter Medium',
+    fontSize: majorThird(1),
+  },
+  buttonTouchable: {
+    backgroundColor: black(),
+    borderRadius: fi(0.5),
+    padding: fi(1),
+  },
+  container: {
+    alignItems: 'center',
+    flex: 1,
+    justifyContent: 'center',
+  },
+  errorCode: {
+    fontFamily: 'Inter Semibold',
+    fontSize: majorThird(5),
+    marginBottom: fi(3),
+  },
+});
--- screens/Index.tsx
+++ screens/Index.tsx
@@ -1,54 +1,54 @@
 import React from 'react';
-import styled from 'styled-components/native';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 import { fi, lineHeight, majorThird } from '../styles/typography';
 import { black, canvasWhite, white } from '../styles/colors';
 
-const Container = styled(View)`
-  flex: 1;
-  justify-content: center;
-  align-items: center;
-  background-color: ${canvasWhite()};
-  height: 100%;
-`;
-
-const Header = styled(View)`
-  position: absolute;
-  top: 0;
-  right: 0;
-  left: 0;
-  background-color: ${white()};
-  width: 100%;
-  flex-direction: row;
-  align-items: center;
-  padding: ${fi(1)}px;
-  border-bottom-width: 0.5px;
-  border-bottom-color: ${black(10)};
-`;
-
-const HeaderText = styled(Text)`
-  font-family: 'Inter Bold';
-  font-size: ${majorThird(1)}px;
-  color: ${black()};
-  text-align: center;
-`;
-
-const Instructions = styled(Text)`
-  font-family: 'Inter SemiBold';
-  font-size: ${majorThird(1)}px;
-  line-height: ${lineHeight(majorThird(1), 1.5)}px;
-  color: ${black()};
-  text-align: center;
-  margin-horizontal: ${fi(1)}px;
-`;
-
 export default function IndexScreen() {
   return (
-    <Container>
-      <Header>
-        <HeaderText>🥤Design Starter Kit</HeaderText>
-      </Header>
-      <Instructions>Open up screens/Index.tsx to start working on your app!</Instructions>
-    </Container>
+    <View style={styles.container}>
+      <View style={styles.headerContainer}>
+        <Text style={styles.headerText}>🥤Design Starter Kit</Text>
+      </View>
+      <Text style={styles.instructions}>
+        Open up screens/Index.tsx to start working on your app!
+      </Text>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    backgroundColor: canvasWhite(),
+    flex: 1,
+    height: '100%',
+    justifyContent: 'center',
+  },
+  headerContainer: {
+    alignItems: 'center',
+    backgroundColor: white(),
+    borderBottomColor: black(10),
+    borderBottomWidth: 0.5,
+    flexDirection: 'row',
+    left: 0,
+    padding: fi(1),
+    position: 'absolute',
+    right: 0,
+    top: 0,
+    width: '100%',
+  },
+  headerText: {
+    color: black(),
+    fontFamily: 'Inter Bold',
+    fontSize: majorThird(1),
+    textAlign: 'center',
+  },
+  instructions: {
+    color: black(),
+    fontFamily: 'Inter SemiBold',
+    fontSize: majorThird(1),
+    lineHeight: lineHeight(majorThird(1), 1.5),
+    marginHorizontal: fi(1),
+    textAlign: 'center',
+  },
+});
